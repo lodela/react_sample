c9d2070dcbc75ab2c39d2fff471c2f525fa517d3
Norberto <lodela.n@gmail.com>
2017-12-20 11:17:32 -0600

    update

--- src/App.js
+++ src/App.js
@@ -68,15 +68,7 @@ class App extends Component {
       shouldHide:true
     })
   }
-  handleSubmit(event){
-    event.preventDefault();
-    this.props.onAddArtist(this.state);
-    this.setState({
-      artist:'',
-      name:'',
-      sales:''
-    });
-  }
+
   render() {
     return (
       <div className="container">
@@ -150,6 +142,27 @@ class AgregaArtista extends Component{
       name  :'',
       sales :''
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit      = this.handleSubmit.bind(this);
+  }
+  handleInputChange(event){
+    const target = event.target;
+    const value  = target.value;
+    const name   = target.name;
+
+    this.setState({
+      [name]:value
+    })
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(this.state);
+    // this.props.onAddArtist(this.state);
+    this.setState({
+      artist:'',
+      name:'',
+      sales:''
+    });
   }
   render(){
     return(
